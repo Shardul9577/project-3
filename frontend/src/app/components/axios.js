e23e19b40a26b925f4835792163c11921559029c
Shardul Chaudhary <shardul1210@gmail.com>
2026-02-15 10:35:43 +0530

    third commit

--- frontend/src/app/components/axios.js
+++ frontend/src/app/components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL = "https://project-3-kdqc.onrender.com/api/v1";
 const TOKEN_STORAGE_KEY = "token";
 
 const api = axios.create({
